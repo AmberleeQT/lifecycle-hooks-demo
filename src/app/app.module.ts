a120c64342dc7fc6bb83adbc26477e1585125576
Amberlee <amberlee.allmond@quotumtech.com>
2022-06-08 12:28:38 -0400

    Initial componets created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AdditemComponent } from './additem/additem.component';
+import { DisplayItemComponent } from './display/display-item/display-item.component';
+import { DisplayCardComponent } from './display/display-card/display-card.component';
+import { ErrorComponent } from './error/error.component';
+import { ErrorMessageComponent } from './error/error-message/error-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdditemComponent,
+    DisplayItemComponent,
+    DisplayCardComponent,
+    ErrorComponent,
+    ErrorMessageComponent
   ],
   imports: [
     BrowserModule,
